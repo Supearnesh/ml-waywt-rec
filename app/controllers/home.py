e851b2eaeddd67caff7d9ed30008d3d87be5a449
Calen Robinette <calen.robinette@gatech.edu>
2020-10-18 02:53:25 -0700

    Images upload and recommendation is presented

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
+import random
 
-from app import app
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, current_app
 from app.utils import allowed_file
 
 blueprint = Blueprint('home', __name__)
@@ -10,25 +10,31 @@ blueprint = Blueprint('home', __name__)
 @blueprint.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if 'file' not in request.files:
+        if 'file1' not in request.files:
             flash('No file part')
             return redirect(url_for('home.index'))
-        file = request.files['file']
+        
+        num = random.randint(1,3)
+        file = request.files[f'file{num}']
         if file.filename == '':
             flash('No image selected for uploading')
             return redirect(url_for('home.index'))
         if file and allowed_file(file.filename):
             filename = file.filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
             #print('upload_image filename: ' + filename)
-            flash('Image successfully uploaded')
-            return render_template('upload.html', filename=filename)
+            flash('You should wear this!')
+            return render_template('home/index.html', filename=filename)
         else:
             flash('Allowed image types are -> png, jpg, jpeg, gif')
             return redirect(url_for('home.index'))
             
 
     return render_template('home/index.html')
+
+@blueprint.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename='uploads/' + filename, code=301))
 
 @blueprint.route('/about')
 def about():
